5da53a520f64f75490a16c12e57bca3bfa5c0d6b
Gesa <olgersa.korra@cit.edu.al>
2025-05-24 16:07:58 +0200

    update

--- app/main.py
+++ app/main.py
@@ -1,8 +1,26 @@
 #app/main.py
 from flask import Flask, render_template
-from app.models import fetch_weather_data
+import psycopg2
 
 app = Flask(__name__)
+
+import psycopg2
+
+def get_connection():
+    return psycopg2.connect(
+        host="10.2.0.4",  # Replace with internal load balancer IP
+        database="weatherapp",
+        user="admin",
+        password="admin1234"
+    )
+def fetch_weather_data():
+    conn = get_connection()
+    cur = conn.cursor()
+    cur.execute("SELECT city, temperature, timestamp FROM weather_data ORDER BY timestamp DESC LIMIT 10")
+    rows = cur.fetchall()
+    cur.close()
+    conn.close()
+    return rows
 
 @app.route("/")
 def home():
