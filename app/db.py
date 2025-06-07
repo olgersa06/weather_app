836b7c0b98d294cf2c8e1ba1efbc9ff9eca5fdba
olgersa06 <olgersakorra@gmail.com>
2025-06-07 13:52:16 +0200

    Update db.py

--- app/db.py
+++ app/db.py
@@ -3,7 +3,7 @@ import psycopg2
 
 def get_connection():
     return psycopg2.connect(
-        host="10.2.0.4",  # Replace with internal load balancer IP
+        host="10.2.1.4",  # Replace with internal load balancer IP
         database="weatherapp",
         user="admin",
         password="admin1234"
